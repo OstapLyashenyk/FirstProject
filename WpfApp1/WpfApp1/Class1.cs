1184fa9b480bfbd7c59811b8ec651602249ec05d
OstapLyashenyk <56645527+OstapLyashenyk@users.noreply.github.com>
2019-11-12 15:47:08 +0200

    Add files via upload

---
+++ WpfApp1/WpfApp1/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Product
+    {
+        int id { get; set; }
+        string name { get; set; }
+        string сategory { get; set; }
+
+        int date { get; set; }
+
+        int expiration { get; set; }
+
+        int count { get; set; }
+
+        int cost { get; set; }
+
+        string firm { get; set; }
+
+        int telephone { get; set; }
+
+        int nextDate { get; set; }
+
+        int storageNumber { get; set; }
+
+        string aBitAbout { get; set; }
+
+        string leftInfo{ get; set; }
+
+}
+}
